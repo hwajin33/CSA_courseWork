a3ca12b41d3be3501ee92920e334bbc2e9b944be
Hwajin Kang <hwajinkang@Hwajinui-MacBookPro.local>
2021-11-26 11:03:25 +0000

    newup

--- gol/distributor.go
+++ gol/distributor.go
@@ -14,9 +14,6 @@ type distributorChannels struct {
 	ioInput    <-chan uint8
 }
 
-
-
-
 const alive = 255
 const dead = 0
 
@@ -24,7 +21,7 @@ func mod(x, m int) int {
 	return (x + m) % m
 }
 
-func calculateNeighbours(p Params, x, y int, world [][]byte) int {
+func calculateNeighbourCells(p Params, x, y int, world [][]byte) int {
 	neighbours := 0
 	for i := -1; i <= 1; i++ {
 		for j := -1; j <= 1; j++ {
@@ -39,14 +36,15 @@ func calculateNeighbours(p Params, x, y int, world [][]byte) int {
 }
 
 
-func calculateNextState(p Params, currentTurnWorld [][]byte) [][]byte {
-	newWorld := make([][]byte, p.ImageHeight)
+func calculateNextState(startY, endY, startX, endX int, p Params, currentTurnWorld [][]byte) [][]byte {
+	newWorld := make([][]byte, endY - startY)
 	for i := range newWorld {
-		newWorld[i] = make([]byte, p.ImageWidth)
+		newWorld[i] = make([]byte, endX - startX)
 	}
-	for y := 0; y < p.ImageHeight; y++ {
-		for x := 0; x < p.ImageWidth; x++ {
-			neighbours := calculateNeighbours(p, x, y, currentTurnWorld)
+
+	for y := startY; y < endY; y++ {
+		for x := startX; x < endX; x++ {
+			neighbours := calculateNeighbourCells(p, x, y, currentTurnWorld)
 			if currentTurnWorld[y][x] == alive {
 				if neighbours == 2 || neighbours == 3 {
 					newWorld[y][x] = alive
@@ -68,29 +66,63 @@ func calculateNextState(p Params, currentTurnWorld [][]byte) [][]byte {
 func calculateAliveCells(p Params, world [][]byte) []util.Cell{
 
 	aliveCells := []util.Cell{}
-	var haha util.Cell
+	var cell util.Cell
 
 	for y := 0; y < p.ImageHeight; y++ {
 		for x := 0; x < p.ImageWidth; x++ {
 			if world[y][x] == 255 {
-				haha.X = x
-				haha.Y = y
-				aliveCells = append(aliveCells, haha)
+				cell.X = x
+				cell.Y = y
+				aliveCells = append(aliveCells, cell)
 			}
 		}
 	}
-
 	return aliveCells
 }
 
+//func getImage(p Params, c distributorChannels, im image.Image) [][]byte{
+// use channels to interact with IO
 
+//	world := make([][]byte, p.ImageHeight)
+//	for i := range world {
+//		world[i] = make([]byte, p.ImageWidth)
+//	}
+//
+//	for y := 0; y < p.ImageHeight; y++ {
+//		for x := 0; x < p.ImageWidth; x++ {
+//			receivedImage := <- c.ioInput
+//if receivedImage != 0 {
+//	fmt.Println(x, y)
+//}
+//			world[y][x] = receivedImage
+//		}
+//	}
+//}
 
+func worker(startY, endY, startX, endX int, world [][]byte, out chan<- [][]uint8, p Params) {
+	imagePortion := calculateNextState(startY, endY, startX, endX, p, world)
+	out <- imagePortion
+}
+
+func loadImage (p Params, c distributorChannels, world [][]byte, turn int) {
+	filename := fmt.Sprintf("%vx%v", p.ImageWidth, p.ImageHeight)
+
+	c.ioCommand <- ioInput
+	c.ioFilename <- filename
+
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			c.ioOutput <- world[y][x]
+		}
+	}
+	c.events <- ImageOutputComplete{turn, filename}
+
+}
 
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
 
 	// TODO: Create a 2D slice to store the world.
-
 	currentWorld := make([][]byte, p.ImageHeight)
 	for i := range currentWorld {
 		currentWorld[i] = make([]byte, p.ImageWidth)
@@ -110,21 +142,63 @@ func distributor(p Params, c distributorChannels) {
 	turn := 0
 
 	// TODO: Execute all turns of the Game of Life.
-	for i := 0; i < p.Turns; i++ {
+	for turn < p.Turns  {
 
-		currentWorld = calculateNextState(p, currentWorld)
+		//nextWorld := make([][]byte, 0)
 
+		nextWorld := make([][]byte, p.ImageHeight)
+		for i := range nextWorld {
+			nextWorld[i] = make([]byte, p.ImageWidth)
+		}
 
-		//world = ...
-		//tempworld = copy the previous world
-		//compute new world(world)
-		// TODO: Report the final state using FinalTurnCompleteEvent.
+		if p.Threads == 1 {
+			currentWorld = calculateNextState(0, p.ImageHeight, 0, p.ImageWidth, p, currentWorld)
+		} else {
+			workerHeight := p.ImageHeight / p.Threads
+			//modHeight := (p.ImageHeight) % p.Threads
 
+			out := make([]chan [][]uint8, p.Threads)
+			for j := range out {
+				out[j] = make(chan [][]uint8)
+			}
 
+			currentThread := 0
+
+			for currentThread < p.Threads {
+				// when we reach to the last thread, start from that thread and end at the p.ImageHeight
+				// if we have floating points after we get the worker height && when we reach to the last thread
+				if currentThread == p.Threads - 1 {
+					fmt.Printf("t [%d] threads = %d\n", currentThread, p.Threads)
+					go worker(currentThread * workerHeight, p.ImageHeight, 0, p.ImageWidth, currentWorld, out[currentThread], p)
+				} else {
+					fmt.Printf("b [%d]threads = %d\n", currentThread, p.Threads)
+					go worker(currentThread * workerHeight, (currentThread + 1) * workerHeight, 0, p.ImageWidth, currentWorld, out[currentThread], p)
+				}
+				currentThread++
+
+				// assembling the world
+				//portion := <-out[currentThread]
+				//nextWorld = append(nextWorld, portion...)
+				//currentWorld = nextWorld
+			}
+
+			// make another for loop going up #of threads -> assembling the world
+			for partThread := 0; partThread < p.Threads; partThread++ {
+				portion := <-out[partThread]
+				nextWorld = append(nextWorld, portion...)
+			}
+			// swapping the worlds
+			currentWorld, nextWorld = nextWorld, currentWorld
+		}
+		turn++
 	}
 
 	aliveCell := calculateAliveCells(p, currentWorld)
+	// TODO: Report the final state using FinalTurnCompleteEvent.
 	c.events <- FinalTurnComplete{turn, aliveCell}
+
+	//loadImage(p, c, currentWorld, turn)
+
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
 	<-c.ioIdle
